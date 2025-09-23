f4f75c6b5d72d4b963338993fa3a41fb8f12fee3
jaseelamktgii@gmail <jaseelamktgi@gmail.com>
2025-09-23 17:21:04 +0530

    added assets, fonts,constants

---
+++ lib/core/constants/font_strings.dart
@@ -0,0 +1,14 @@
+//Fonts
+const poppins300 = 'PoppinsLight',
+    poppins400 = "Poppins",
+    poppins500 = "PoppinsMedium",
+    poppins600 = "PoppinsSemiBold",
+    poppins700 = "PoppinsBold",
+    poppins800 = "PoppinsExtraBold";
+
+// assets helper
+abstract class AssetsHelper {
+  static const String images = 'assets/images';
+  static const String logo = '$images/logo.png';
+  static const String splashBg = '$images/splash-bg.jpg';
+}
